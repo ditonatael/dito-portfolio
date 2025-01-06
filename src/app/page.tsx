6f9521dc89c31a83ced5c4caa5967c6e3f3c93dd
ditonatael <158138590+ditonatael@users.noreply.github.com>
2025-01-06 09:34:41 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="container mx-auto py-1">Dito Natael</div>
+      <div className="container mx-auto py-1">Dito Natael Portfolio</div>
     </>
   );
 }
