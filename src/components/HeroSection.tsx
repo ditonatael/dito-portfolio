b4e8dcc374aeb8264c541d4b37edeccae9c1d393
mac <mac@macs-MacBook-Pro.local>
2025-03-04 12:30:41 +0700

    feat: add techstack

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,10 +1,79 @@
 "use client";
+import Image from "next/image";
+import Marquee from "react-fast-marquee";
 import Typewriter from "typewriter-effect";
 
 export default function HeroSection() {
+  const techStack = [
+    {
+      title: "Javascript",
+      icon: "/javascript.webp",
+    },
+    {
+      title: "Typescript",
+      icon: "/typescript.webp",
+    },
+    {
+      title: "React",
+      icon: "/react.webp",
+    },
+    {
+      title: "NextJS",
+      icon: "/nextjs.webp",
+    },
+    {
+      title: "VueJS",
+      icon: "/vuejs.webp",
+    },
+    {
+      title: "NuxtJs",
+      icon: "/nuxtjs.webp",
+    },
+    {
+      title: "TailwindCSS",
+      icon: "/tailwind.webp",
+    },
+    {
+      title: "Redux",
+      icon: "/redux.webp",
+    },
+    {
+      title: "React Query",
+      icon: "/reactquery.webp",
+    },
+    {
+      title: "NodeJS",
+      icon: "/nodejs.webp",
+    },
+    {
+      title: "ExpressJS",
+      icon: "/express.webp",
+    },
+    {
+      title: "MySQL",
+      icon: "/mysql.webp",
+    },
+    {
+      title: "Prisma",
+      icon: "/prisma.webp",
+    },
+    {
+      title: "Git/Github",
+      icon: "/github.webp",
+    },
+    {
+      title: "Postman",
+      icon: "/postman.webp",
+    },
+    {
+      title: "FirebaseAuth",
+      icon: "/firebase.webp",
+    },
+  ];
+
   return (
     <div className="h-screen flex items-center">
-      <div className="flex flex-col gap-2.5">
+      <div className="w-1/2 flex flex-col gap-2.5">
         <span className="text-5xl font-bold text-primary-black">
           Hello 👋, I'm Handito Natael
         </span>
@@ -29,13 +98,24 @@ export default function HeroSection() {
           efficient and scalable applications.
         </span>
         <span className="flex gap-2.5">
-          <button className="w-28 h-10 border-2 border-primary-black rounded-full">
+          <button className="w-28 h-10 border-2 border-primary-black rounded-full hover:bg-primary-black hover:text-white font-medium text-sm">
             Contact Me
           </button>
-          <button className="w-28 h-10 border-2 border-primary-black rounded-full">
+          <button className="w-28 h-10 border-2 border-primary-black rounded-full hover:bg-primary-black hover:text-white font-medium text-sm">
             My Portfolio
           </button>
         </span>
+        <div className="pt-2.5">
+          <Marquee>
+            {techStack.map((item, index) => {
+              return (
+                <div key={index} className="size-12 relative mx-2">
+                  <Image key={index} src={item.icon} fill alt="icon" />
+                </div>
+              );
+            })}
+          </Marquee>
+        </div>
       </div>
     </div>
   );
