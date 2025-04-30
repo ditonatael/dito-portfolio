ce0e85cf1a30d07806670f02a3895ba1f2db7e6b
ditonatael <mac@macs-MacBook-Pro.local>
2025-04-30 14:40:11 +0700

    feat: skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import About from "~/components/About";
 import WorkExperience from "~/components/WorkExperience";
 import Education from "~/components/Education";
+import Skills from "~/components/Skills";
 
 export default function Home() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -14,13 +15,14 @@ export default function Home() {
   return !isLoaded ? (
     <></>
   ) : (
-    <div className="min-h-[100dvh] pt-14 px-4 md:px-0">
+    <div className="min-h-[100dvh] py-14 px-4 md:px-0">
       <div className="mx-auto w-full max-w-2xl flex flex-col gap-6">
         <Hero />
         <About />
         <div className="border"></div>
         <WorkExperience />
         <Education />
+        <Skills />
       </div>
     </div>
   );
